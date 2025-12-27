479d87996f6fb9f7112e8ad9ac2558fe627763e2
NARSI NANDHU <santhoshayyanar268@gmail.com>
2025-12-27 12:27:52 +0530

    nothing

--- Projects/project-1(Shopez)/shopez-frontend/src/features/product/productService.js
+++
@@ -1,6 +0,0 @@
-import api from '../../services/api';
-
-export const fetchProductsApi = async (params = {}) => {
-  const response = await api.get('/products', { params });
-  return response.data;
-};
--- Projects/project-1(Shopez)/shopez-frontend/src/services/paymentService.js
+++
@@ -1,6 +0,0 @@
-import api from './api';
-
-export const dummyPayApi = async (orderId) => {
-  const { data } = await api.post('/payment/dummy', { orderId });
-  return data;
-};
